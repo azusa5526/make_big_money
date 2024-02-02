bc128dd9093c1b085651604825b617324c561e82
AndrewYao <andrewyao@anasystem.com.tw>
2024-02-02 09:19:02 +0800

    feat: Activity 增加 Expense 的顯示

--- lib/mods/activity/view/activity.dart
+++ lib/mods/activity/view/activity.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../expense/view/expense_list_view.dart';
 
 class Activity extends StatelessWidget {
   const Activity({super.key, required this.activityId});
@@ -8,8 +10,18 @@ class Activity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Activity')),
-      body: Text(activityId),
-    );
+        appBar: AppBar(title: const Text('Activity')),
+        body: Container(
+          color: Colors.grey[100],
+          padding: EdgeInsets.all(12.0),
+          child: ExpenseListView(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.pushNamed('expense_add');
+          },
+          shape: const CircleBorder(),
+          child: const Icon(Icons.add),
+        ));
   }
 }
